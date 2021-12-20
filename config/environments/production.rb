d1b4529988b1bb306c1ff344c714f67d93292d93
tazuma <tazuma@newrelic.com>
2021-12-20 17:23:58 +0900

    change default logger

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,8 +82,8 @@ Rails.application.configure do
   config.active_support.disallowed_deprecation_warnings = []
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter = ::Logger::Formatter.new
-
+  # config.log_formatter = ::Logger::Formatter.new
+  config.log_formatter = ::NewRelic::Agent::Logging::DecoratingFormatter.new
   # Use a different logger for distributed setups.
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
