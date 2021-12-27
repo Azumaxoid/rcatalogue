f0a7763503aeef9c30fe12bde987df4229df18b4
tazuma <tazuma@newrelic.com>
2021-12-27 09:58:46 +0900

    update tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,6 +2,14 @@ class TagsController < ApplicationController
   skip_before_action :verify_authenticity_token
   def index
     @tags = Tag.all.map{|tag| tag.name}
-    render json: { "tags": @tags }
+    @count = @tags.size
+    # Green socks tag have a bug so filtered
+    @tags = @tags.filter {|item| item.name.not.equal?("green")}
+    @newTags = []
+    for @i in 0..@count-1
+      @newTags.append(@tags[@i])
+    end
+
+    render json: { "tags": @newTags }
   end
 end
