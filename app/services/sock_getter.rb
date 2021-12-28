66cae9bd4ee37ace593be1c78fb170cf70aebb9f
tazuma <tazuma@newrelic.com>
2021-12-28 20:16:15 +0900

    fix for status

--- app/services/sock_getter.rb
+++ app/services/sock_getter.rb
@@ -8,7 +8,7 @@ class SockGetter
   def call
     @socks = Sock.where(sock_id: @sock_id).where.not(name: nil).where("count > ?", 0)
     if @socks.length == 0
-      return { message: "There are no item. Please select other items", error: true }, :status => :internal_server_error
+      return { message: "There are no item. Please select other items", error: true }
     end
     @sock = @socks[0]
     @tags = SockTag.where(sock_id: @sock_id).map{|sockTag| Tag.find_by(tag_id: sockTag.tag_id).name}
