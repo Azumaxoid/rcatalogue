3f291792e03f509409cdc8707d4776d746b77345
tazuma <tazuma@newrelic.com>
2021-12-29 14:46:31 +0900

    タグにcountを追加

--- app/services/tag_selector.rb
+++ app/services/tag_selector.rb
@@ -2,7 +2,15 @@ class TagSelector
   include Service
 
   def call
-    result = Tag.all.map{|tag| tag.name}
+    tags = Tag.all.map{|tag| tag.name}
+    count = tags.size
+    # Green socks tag have a bug so filtered
+    tags = tags.filter {|item| item != "green"}
+    result = []
+    for @i in 0..count-1
+      result.append(tags.fetch(@i))
+    end
+    result = result.sort{|a, b| a <=> b }
     return result
   end
 
