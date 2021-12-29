5e1d53957f0609e37d7fcb921a124d149adf02ca
tazuma <tazuma@newrelic.com>
2021-12-29 09:08:51 +0900

    fix

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,9 +2,9 @@ class TagsController < ApplicationController
   skip_before_action :verify_authenticity_token
   def index
     @tags = Tag.all.map{|tag| tag.name}
-    @count = @tags.size
     # Green socks tag have a bug so filtered
     @tags = @tags.filter {|item| item != "green"}
+    @count = @tags.size
     @newTags = []
     for @i in 0..@count-1
       @newTags.append(@tags.fetch(@i))
